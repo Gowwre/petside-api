aec92591c5735b524a0161f3a9cf63b59398f7d8
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-01-23 14:28:27 +0700

    fix typo

--- PetHealthCare/Model/DTO/Request/UserDTO.cs
+++ PetHealthCare/Model/DTO/Request/UserDTO.cs
@@ -12,9 +12,9 @@ public class UserDTO
     public string Password { get; set; }
     [Required]
     [Compare("Password", ErrorMessage = "Password and Repeat Password do not match.")]
-    public string RepeatPassword { get; set; }
-    public string Avarta { get; set; }
-    public string BirthDay { get; set; }
+    public string ConfirmPassword { get; set; }
+    public string Avatar { get; set; }
+    public string DateOfBirth { get; set; }
     public string Address { get; set; }
     public string PhoneNumber { get; set; }
 }
