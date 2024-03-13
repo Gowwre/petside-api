cb867e0907be93a906290eaaa91cdab7be0e9307
LuongHuynhNgocHao <Haoluonghuynhngoc@gmail.com>
2024-03-13 21:35:54 +0700

    Update Register

--- PetHealthCare/Model/DTO/Request/UserRegistrationDto.cs
+++ PetHealthCare/Model/DTO/Request/UserRegistrationDto.cs
@@ -6,14 +6,14 @@ public class UserRegistrationDto
 {
     [Required] public string FullName { get; set; }
 
-    [Required] [EmailAddress] public string Email { get; set; }
+    [Required][EmailAddress] public string Email { get; set; }
 
     [Required] public string Password { get; set; }
 
-    [Required]
-    [Compare("Password", ErrorMessage = "Password and Repeat Password do not match.")]
-    public string RepeatPassword { get; set; }
+    //[Required]
+    //[Compare("Password", ErrorMessage = "Password and Repeat Password do not match.")]
+    //public string RepeatPassword { get; set; }
 
-    [RegularExpression(@"(09|03|07|08|05)([0-9]{8})\b", ErrorMessage = "Invalid Phone Number")]
+    //[RegularExpression(@"(09|03|07|08|05)([0-9]{8})\b", ErrorMessage = "Invalid Phone Number")]
     public string? PhoneNumber { get; set; }
 }
